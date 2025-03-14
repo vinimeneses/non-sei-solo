51162c8d9398d3a55ec5e814f1192dedc19aa0ce
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-03-14 17:03:21 -0300

    removing .env traces git

--- api/go.mod
+++ api/go.mod
@@ -3,6 +3,7 @@ module api
 go 1.20
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/labstack/echo/v4 v4.13.3 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- api/main.go
+++ api/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dns := os.Getenv("DB_URL")
+	fmt.Println(dns)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
