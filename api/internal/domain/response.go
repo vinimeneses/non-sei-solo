7b69a4817a89bdeee211ad5ccf1f70e9fcd448fe
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2025-03-14 16:00:45 -0300

    running echo framework

---
+++ api/internal/domain/response.go
@@ -0,0 +1,18 @@
+package domain
+
+type Response struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
+
+type PaginatedResponse struct {
+	Status  int         `json:"status"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+	Page    int         `json:"page"`
+	Limit   int         `json:"limit"`
+	Total   int         `json:"total"`
+}
